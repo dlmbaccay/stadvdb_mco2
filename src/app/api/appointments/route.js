3fd950564236edca1214db14da796c4e04fe9ca0
Andre <andre_aquino@dlsu.edu.ph>
2024-04-09 23:26:56 +0800

    Fixed insert new appointment

--- src/app/api/appointments/route.js
+++ src/app/api/appointments/route.js
@@ -1,66 +1,111 @@
-import { NextResponse } from "next/server";
-import {pool1} from "@/lib/database";
-import { uuid } from 'uuidv4';
+import { NextResponse } from 'next/server'
+import { pool1 } from '@/lib/database'
+import { v4 as uuidv4 } from 'uuid'
 
 export async function GET(request) {
-    const { searchParams } = new URL(request.url);
-    const querypage = searchParams.get('page');
-    const page = querypage ? parseInt(querypage) : 1;
-    const pageSize = 10; // Number of records per page
-    const offset = (page - 1) * pageSize;
+	const { searchParams } = new URL(request.url)
+	const querypage = searchParams.get('page')
+	const page = querypage ? parseInt(querypage) : 1
+	const pageSize = 10 // Number of records per page
+	const offset = (page - 1) * pageSize
 
-    try {
-        const db = await pool1.getConnection();
-        const query = `SELECT * FROM appointments LIMIT ${pageSize} OFFSET ${offset}`; // Add WHERE conditions
-        const [rows] = await db.execute(query);
-        db.release();
+	try {
+		const db = await pool1.getConnection()
+		const query = `SELECT * FROM appointments LIMIT ${pageSize} OFFSET ${offset}` // Add WHERE conditions
+		const [rows] = await db.execute(query)
+		db.release()
 
-        return NextResponse.json(rows);
-    } catch (error) {
-        return NextResponse.json({
-            error: error
-        }, { status: 500 })
-    }
-};
-
-export async function POST(request) {
-    const body = await request.json();
-    const { appointmentData } = body;
-
-    let appointmentID;
-
-    try {
-        const db = await pool1.getConnection()
-
-         // Keep generating a new unique ID until a unique one is found
-         while (!isUniqueID) {
-            appointmentID = uuidv4(); // Generate a unique ID
-            const checkQuery = 'SELECT COUNT(*) AS count FROM appointments WHERE appointmentID = ? LIMIT 1';
-            const [checkRows] = await db.execute(checkQuery, [appointmentID]);
-
-            if (checkRows[0].count === 0) {
-                isUniqueID = true; // Unique ID found
-            }
-        }
-
-        // Format the UUID to match the desired format
-        appointmentID = formatUUID(appointmentID);
-
-        // Insert data into the database using the generated unique ID
-        const insertQuery = 'INSERT INTO appointments (appointmentID, appointmentStatus, appointmentDate /* Add other columns */) VALUES (?, ?, ? /* Add other placeholders */)';
-        const [insertRows] = await db.execute(insertQuery, [appointmentID, appointmentStatus, appointmentDate /* Add other values */]);
-
-        db.release();
-        
-        return NextResponse.json(insertRows)
-    } catch (error) {
-        return NextResponse.json({
-            error: error
-        }, { status: 500 })
-    }
+		return NextResponse.json(rows)
+	} catch (error) {
+		return NextResponse.json(
+			{
+				error: error,
+			},
+			{ status: 500 },
+		)
+	}
 }
 
-function formatUUID(uuid) {
-    // Remove hyphens from the UUID and convert it to uppercase
-    return uuid.replace(/-/g, '').toUpperCase();
+export async function POST(request) {
+	const body = await request.json()
+	const {
+		appointmentStatus,
+		appointmentType,
+		timeQueued,
+		queueDate,
+		startTime,
+		endTime,
+		virtual,
+		hospitalName,
+		city,
+		province,
+		region,
+		doctorMainSpecialty,
+		doctorAge,
+		patientAge,
+		patientGender,
+	} = body
+
+	// let appointmentID
+
+	try {
+		const db = await pool1.getConnection()
+
+		// Start transaction
+		await db.beginTransaction()
+
+		// Generate UUID in the desired format
+		let apptid = uuidv4().toUpperCase().replace(/-/g, '').substring(0, 32)
+
+		let exists = true
+
+		while (exists) {
+			console.log(apptid)
+			const checkQuery = 'SELECT COUNT(*) AS count FROM appointments WHERE apptid = ? LIMIT 1'
+			const [checkRows] = await db.execute(checkQuery, [apptid])
+			exists = checkRows[0].count > 0
+
+			if (exists) {
+				apptid = uuidv4().toUpperCase().replace(/-/g, '').substring(0, 32)
+			}
+		}
+
+		const insertQuery =
+			'INSERT INTO appointments (apptid, appt_status, appt_type, time_queued, queue_date, start_time, end_time, appt_virtual, patient_age, patient_gender, doctor_age, doctor_mainspecialty, hospital_name, city, province, region) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+		const [insertRows] = await db.execute(insertQuery, [
+			apptid,
+			appointmentStatus,
+			appointmentType,
+			timeQueued,
+			queueDate,
+			startTime,
+			endTime,
+			virtual,
+			patientAge,
+			patientGender,
+			doctorAge,
+			doctorMainSpecialty,
+			hospitalName,
+			city,
+			province,
+			region,
+		])
+
+		// Commit transaction
+		await db.commit()
+
+		db.release()
+
+		return NextResponse.json(insertRows)
+	} catch (error) {
+		// Rollback transaction if error occurs
+		await db.rollback()
+		console.error('Error inserting appointment:', error)
+		return NextResponse.json(
+			{
+				error: error,
+			},
+			{ status: 500 },
+		)
+	}
 }
--- src/components/insert-form.js
+++ src/components/insert-form.js
@@ -18,6 +18,11 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select'
+import { format } from 'date-fns'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import { Calendar } from '@/components/ui/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { DatePicker } from './ui/date-picker'
@@ -48,9 +53,34 @@ export function CreateAppointment() {
 
 		setLoading(true)
 		try {
+			const response = await fetch('/api/appointments', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					appointmentStatus,
+					appointmentType,
+					timeQueued,
+					queueDate,
+					startTime,
+					endTime,
+					virtual,
+					hospitalName,
+					city,
+					province,
+					region,
+					doctorMainSpecialty,
+					doctorAge,
+					patientAge,
+					patientGender,
+				}),
+			})
+			const data = await response.json()
+			console.log(data)
+
 			setLoading(false)
 			setOpen(false)
-			router.refresh()
 		} catch (error) {
 			console.error(error)
 			setLoading(false)
@@ -65,176 +95,207 @@ export function CreateAppointment() {
 					<Button className="border border-primary hover:text-background">Insert</Button>
 				</DialogTrigger>
 			</div>
-			<DialogContent className="sm:max-w-[600px] lg:max-w-[720px]">
+			<DialogContent className="w-5/6 max-w-5/6">
 				<DialogHeader>
 					<DialogTitle className="text-center">Create an Appointment</DialogTitle>
 				</DialogHeader>
 				<hr className="border-primary mx-4 my-2 border-b" />
 				<form onSubmit={createAppointment}>
-					<div className="mb-4 flex w-full flex-col">
-						<div className="flex w-full flex-row gap-12 px-10">
-							<div className="flex flex-col w-1/2">
-								<Label>Appointment Status</Label>
-								<Select
-									required
-									onValueChange={setAppointmentStatus}
-									defaultValue={appointmentStatus}
-								>
-									<SelectTrigger>
-										<SelectValue placeholder="Select Status">
-											{appointmentStatus}
-										</SelectValue>
-									</SelectTrigger>
-									<SelectContent>
-										<SelectItem value="Complete">Complete</SelectItem>
-										<SelectItem value="Queued">Queued</SelectItem>
-										<SelectItem value="Serving">Serving</SelectItem>
-										<SelectItem value="Cancel">Cancel</SelectItem>
-										<SelectItem value="NoShow">No Show</SelectItem>
-										<SelectItem value="Skip">Skip</SelectItem>
-									</SelectContent>
-								</Select>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Appointment Type</Label>
-								<Select>
-									<SelectTrigger>
-										<SelectValue>{appointmentType}</SelectValue>
-									</SelectTrigger>
-									<SelectContent>
-										<SelectItem
-											onClick={() => setAppointmentType('Consultation')}
+					<div className="mb-4 flex w-full">
+						<div className="flex w-full flex-row gap-4 px-10 flex-wrap">
+							<div className="w-full flex flex-col p-8 gap-4 border border-primary rounded-lg">
+								<h1 className="font-semibold">Appointment Details</h1>
+								<div className="flex flex-row gap-8">
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Appointment Status</Label>
+										<Select
+											required
+											onValueChange={setAppointmentStatus}
+											defaultValue={appointmentStatus}
 										>
-											Consultation
-										</SelectItem>
-										<SelectItem onClick={() => setAppointmentType('Inpatient')}>
-											Inpatient
-										</SelectItem>
-									</SelectContent>
-								</Select>
+											<SelectTrigger>
+												<SelectValue placeholder="Select Status">
+													{appointmentStatus}
+												</SelectValue>
+											</SelectTrigger>
+											<SelectContent>
+												<SelectItem value="Complete">Complete</SelectItem>
+												<SelectItem value="Queued">Queued</SelectItem>
+												<SelectItem value="Serving">Serving</SelectItem>
+												<SelectItem value="Cancel">Cancel</SelectItem>
+												<SelectItem value="NoShow">No Show</SelectItem>
+												<SelectItem value="Skip">Skip</SelectItem>
+											</SelectContent>
+										</Select>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Appointment Type</Label>
+										<Select
+											required
+											onValueChange={setAppointmentType}
+											defaultValue={appointmentType}
+										>
+											<SelectTrigger>
+												<SelectValue placeholder="Select Type">
+													{appointmentType}
+												</SelectValue>
+											</SelectTrigger>
+											<SelectContent>
+												<SelectItem value="Consultation">
+													Consultation
+												</SelectItem>
+												<SelectItem value="Inpatient">Inpatient</SelectItem>
+											</SelectContent>
+										</Select>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Virtual</Label>
+										<Select
+											required
+											onValueChange={setVirtual}
+											defaultValue={virtual}
+										>
+											<SelectTrigger>
+												<SelectValue placeholder="Is Virtual? ">
+													{virtual}
+												</SelectValue>
+											</SelectTrigger>
+											<SelectContent>
+												<SelectItem value="True">True</SelectItem>
+												<SelectItem value="False">False</SelectItem>
+											</SelectContent>
+										</Select>
+									</div>
+								</div>
+								<div className="flex flex-row gap-8 flex-wrap">
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Time Queued</Label>
+										<Input
+											required
+											type="datetime-local"
+											value={timeQueued}
+											onChange={(e) => setTimeQueued(e.target.value)}
+										></Input>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Queue Date</Label>
+										<Input
+											required
+											type="datetime-local"
+											value={queueDate}
+											onChange={(e) => setQueueDate(e.target.value)}
+										></Input>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Start Time</Label>
+										<Input
+											required
+											type="datetime-local"
+											value={startTime}
+											onChange={(e) => setStartTime(e.target.value)}
+										></Input>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">End Time</Label>
+										<Input
+											required
+											type="datetime-local"
+											value={endTime}
+											onChange={(e) => setEndTime(e.target.value)}
+										></Input>
+									</div>
+								</div>
 							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Time Queued</Label>
-								<DatePicker
-									mode="single"
-									selected={timeQueued}
-									onSelect={setTimeQueued}
-								/>
+							<div className="w-full flex flex-col flex-wrap p-8 gap-4 border border-primary rounded-lg">
+								<h1 className="font-semibold">Clinic Details</h1>
+								<div className="flex flex-row gap-8 flex-wrap">
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Hospital Name</Label>
+										<Input
+											required
+											type="text"
+											value={hospitalName}
+											onChange={(e) => setHospitalName(e.target.value)}
+										/>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">City</Label>
+										<Input
+											required
+											type="text"
+											value={city}
+											onChange={(e) => setCity(e.target.value)}
+										/>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Province</Label>
+										<Input
+											required
+											type="text"
+											value={province}
+											onChange={(e) => setProvince(e.target.value)}
+										/>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Region</Label>
+										<Input
+											required
+											type="text"
+											value={region}
+											onChange={(e) => setRegion(e.target.value)}
+										/>
+									</div>
+								</div>
 							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Queue Date</Label>
-								<Input
-									type="text"
-									value={queueDate}
-									onChange={(e) => setQueueDate(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Start Time</Label>
-								<Input
-									type="text"
-									value={startTime}
-									onChange={(e) => setStartTime(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>End Time</Label>
-								<Input
-									type="text"
-									value={endTime}
-									onChange={(e) => setEndTime(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Virtual</Label>
-								<Select>
-									<SelectTrigger>
-										<SelectValue>{virtual}</SelectValue>
-									</SelectTrigger>
-									<SelectContent>
-										<SelectItem onClick={() => setVirtual('True')}>
-											True
-										</SelectItem>
-										<SelectItem onClick={() => setVirtual('False')}>
-											False
-										</SelectItem>
-									</SelectContent>
-								</Select>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Hospital Name</Label>
-								<Input
-									type="text"
-									value={hospitalName}
-									onChange={(e) => setHospitalName(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>City</Label>
-								<Input
-									type="text"
-									value={city}
-									onChange={(e) => setCity(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Province</Label>
-								<Input
-									type="text"
-									value={province}
-									onChange={(e) => setProvince(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Region</Label>
-								<Input
-									type="text"
-									value={region}
-									onChange={(e) => setRegion(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Doctor Main Specialty</Label>
-								<Input
-									type="text"
-									value={doctorMainSpecialty}
-									onChange={(e) => setDoctorMainSpecialty(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Doctor Age</Label>
-								<Input
-									type="text"
-									value={doctorAge}
-									onChange={(e) => setDoctorAge(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Patient Age</Label>
-								<Input
-									type="text"
-									value={patientAge}
-									onChange={(e) => setPatientAge(e.target.value)}
-								/>
-							</div>
-							<div className="flex flex-col w-1/2">
-								<Label>Patient Gender</Label>
-								<Select>
-									<SelectTrigger>
-										<SelectValue>{patientGender}</SelectValue>
-									</SelectTrigger>
-									<SelectContent>
-										<SelectItem onClick={() => setPatientGender('Male')}>
-											Male
-										</SelectItem>
-										<SelectItem onClick={() => setPatientGender('Female')}>
-											Female
-										</SelectItem>
-										<SelectItem onClick={() => setPatientGender('Other')}>
-											Other
-										</SelectItem>
-									</SelectContent>
-								</Select>
+							<div className="w-full flex flex-col flex-wrap p-8 gap-4 border border-primary rounded-lg">
+								<h1 className="font-semibold">Doctor and Patient Details</h1>
+								<div className="flex flex-row gap-8 flex-wrap">
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Doctor Main Specialty</Label>
+										<Input
+											required
+											type="text"
+											value={doctorMainSpecialty}
+											onChange={(e) => setDoctorMainSpecialty(e.target.value)}
+										/>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Doctor Age</Label>
+										<Input
+											required
+											type="number"
+											value={doctorAge}
+											onChange={(e) => setDoctorAge(e.target.value)}
+										/>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Patient Age</Label>
+										<Input
+											required
+											type="number"
+											value={patientAge}
+											onChange={(e) => setPatientAge(e.target.value)}
+										/>
+									</div>
+									<div className="flex flex-col w-fit">
+										<Label className="py-2">Patient Gender</Label>
+										<Select
+											required
+											onValueChange={setPatientGender}
+											defaultValue={patientGender}
+										>
+											<SelectTrigger>
+												<SelectValue placeholder="Select Gender ">
+													{patientGender}
+												</SelectValue>
+											</SelectTrigger>
+											<SelectContent>
+												<SelectItem value="MALE">Male</SelectItem>
+												<SelectItem value="FEMALE">Female</SelectItem>
+											</SelectContent>
+										</Select>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -243,7 +304,7 @@ export function CreateAppointment() {
 							<Button disabled>Please wait</Button>
 						) : (
 							<Button type="submit" className="mt-6">
-								Create Post
+								Create Appointment
 							</Button>
 						)}
 					</DialogFooter>
