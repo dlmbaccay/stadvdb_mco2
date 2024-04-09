6802ad744404b5bec489873e2e7f88af9ab3d5bf
Andre <andre_aquino@dlsu.edu.ph>
2024-04-09 22:17:54 +0800

    UI components added

--- src/app/page.js
+++ src/app/page.js
@@ -5,35 +5,35 @@ import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { ModeToggle } from '@/components/ui/mode-toggle'
 import AppointmentTable from '@/components/appointment-table'
+import { CreateAppointment } from '@/components/insert-form'
 
 export default function Home() {
-    const [appointments, setAppointments] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
+	const [appointments, setAppointments] = useState([])
+	const [currentPage, setCurrentPage] = useState(1)
 
-    useEffect(() => {
-        fetchData();
-    }, [currentPage]);
+	useEffect(() => {
+		fetchData()
+	}, [currentPage])
 
-    async function fetchData() {
-        try {
-            const response = await fetch(`/api/appointments?page=${currentPage}`);
-            const data = await response.json();
-            setAppointments(data);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    }
+	async function fetchData() {
+		try {
+			const response = await fetch(`/api/appointments?page=${currentPage}`)
+			const data = await response.json()
+			setAppointments(data)
+		} catch (error) {
+			console.error('Error fetching data:', error)
+		}
+	}
 
-    function incrementPage() {
-        setCurrentPage(currentPage + 1);
-    }
+	function incrementPage() {
+		setCurrentPage(currentPage + 1)
+	}
 
-    function decrementPage() {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    }
-
+	function decrementPage() {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1)
+		}
+	}
 
 	return (
 		<main className="flex flex-col bg-background h-lvh w-full md:p-12 p-4">
@@ -45,9 +45,7 @@ export default function Home() {
 				<Card className="md:w-2/3 w-full h-full p-6 text-black rounded-[1rem] shadow-lg gap-4 justify-between flex flex-col border border-primary">
 					<div className="flex flex-row w-full h-fit p-2 gap-x-6 gap-y-2 items-center flex-wrap justify-between">
 						<div className="flex flex-row h-fit p-2 gap-x-6 gap-y-2 items-center flex-wrap">
-							<Button className="border border-primary hover:text-background">
-								Insert
-							</Button>
+							<CreateAppointment />
 							<Button className="border border-primary hover:text-background">
 								Search
 							</Button>
@@ -59,10 +57,16 @@ export default function Home() {
 							<span className="text-sm text-primary dark:text-primary-foreground">
 								Showing 1-10 of 100
 							</span>
-							<Button onClick={decrementPage} className="border border-primary hover:text-background">
+							<Button
+								onClick={decrementPage}
+								className="border border-primary hover:text-background"
+							>
 								← Prev
 							</Button>
-							<Button onClick={incrementPage} className="border border-primary hover:text-background">
+							<Button
+								onClick={incrementPage}
+								className="border border-primary hover:text-background"
+							>
 								Next →
 							</Button>
 						</div>
--- src/components/insert-form.js
+++ src/components/insert-form.js
@@ -20,35 +20,34 @@ import {
 } from '@/components/ui/select'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { Textarea } from '@/components/ui/textarea'
+import { DatePicker } from './ui/date-picker'
 
 export function CreateAppointment() {
 	const router = useRouter()
 	const [loading, setLoading] = useState(false)
 	const [open, setOpen] = useState(false)
 
-    const [appointmentStatus, setAppointmentStatus] = useState('')
-    const [appointmentType, setAppointmentType] = useState('')
-    const [timeQueued, setTimeQueued] = useState('')
-    const [queueDate, setQueueDate] = useState('')
-    const [startTime, setStartTime] = useState('')
-    const [endTime, setEndTime] = useState('')
-    const [virtual, setVirtual] = useState('')
-    const [hospitalName, setHospitalName] = useState('')
-    const [city, setCity] = useState('')
-    const [province, setProvince] = useState('')
-    const [region, setRegion] = useState('')
-    const [doctorMainSpecialty, setDoctorMainSpecialty] = useState('')
-    const [doctorAge, setDoctorAge] = useState('')
-    const [patientAge, setPatientAge] = useState('')
-    const [patientGender, setPatientGender] = useState('')
+	const [appointmentStatus, setAppointmentStatus] = useState('Complete')
+	const [appointmentType, setAppointmentType] = useState('Consultation')
+	const [timeQueued, setTimeQueued] = useState('')
+	const [queueDate, setQueueDate] = useState('')
+	const [startTime, setStartTime] = useState('')
+	const [endTime, setEndTime] = useState('')
+	const [virtual, setVirtual] = useState('')
+	const [hospitalName, setHospitalName] = useState('')
+	const [city, setCity] = useState('')
+	const [province, setProvince] = useState('')
+	const [region, setRegion] = useState('')
+	const [doctorMainSpecialty, setDoctorMainSpecialty] = useState('')
+	const [doctorAge, setDoctorAge] = useState('')
+	const [patientAge, setPatientAge] = useState('')
+	const [patientGender, setPatientGender] = useState('')
 
 	const createAppointment = async (event) => {
 		event.preventDefault()
 
 		setLoading(true)
 		try {
-            
 			setLoading(false)
 			setOpen(false)
 			router.refresh()
@@ -63,9 +62,7 @@ export function CreateAppointment() {
 			{/* Trigger Buttons */}
 			<div className="flex flex-row items-center">
 				<DialogTrigger asChild>
-                <Button className="border border-primary hover:text-background">
-								Insert
-							</Button>
+					<Button className="border border-primary hover:text-background">Insert</Button>
 				</DialogTrigger>
 			</div>
 			<DialogContent className="sm:max-w-[600px] lg:max-w-[720px]">
@@ -75,109 +72,175 @@ export function CreateAppointment() {
 				<hr className="border-primary mx-4 my-2 border-b" />
 				<form onSubmit={createAppointment}>
 					<div className="mb-4 flex w-full flex-col">
-                        <div className="flex w-full flex-row gap-12 px-10">
-                            <div className="flex flex-col w-1/2">
-                                <Label>Appointment Status</Label>
-                                <Select>
-                                    <SelectTrigger>
-                                        <SelectValue>{appointmentStatus}</SelectValue>
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem onClick={() => setAppointmentStatus('Pending')}>Pending</SelectItem>
-                                        <SelectItem onClick={() => setAppointmentStatus('Confirmed')}>Confirmed</SelectItem>
-                                        <SelectItem onClick={() => setAppointmentStatus('Cancelled')}>Cancelled</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Appointment Type</Label>
-                                <Select>
-                                    <SelectTrigger>
-                                        <SelectValue>{appointmentType}</SelectValue>
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem onClick={() => setAppointmentType('General Checkup')}>General Checkup</SelectItem>
-                                        <SelectItem onClick={() => setAppointmentType('Specialist Consultation')}>Specialist Consultation</SelectItem>
-                                        <SelectItem onClick={() => setAppointmentType('Diagnostic Test')}>Diagnostic Test</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Time Queued</Label>
-                                <Input type="text" value={timeQueued} onChange={(e) => setTimeQueued(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Queue Date</Label>
-                                <Input type="text" value={queueDate} onChange={(e) => setQueueDate(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Start Time</Label>
-                                <Input type="text" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>End Time</Label>
-                                <Input type="text" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Virtual</Label>
-                                <Select>
-                                    <SelectTrigger>
-                                        <SelectValue>{virtual}</SelectValue>
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem onClick={() => setVirtual('Yes')}>Yes</SelectItem>
-                                        <SelectItem onClick={() => setVirtual('No')}>No</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Hospital Name</Label>
-                                <Input type="text" value={hospitalName} onChange={(e) => setHospitalName(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>City</Label>
-                                <Input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Province</Label>
-                                <Input type="text" value={province} onChange={(e) => setProvince(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Region</Label>
-                                <Input type="text" value={region} onChange={(e) => setRegion(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Doctor Main Specialty</Label>
-                                <Input type="text" value={doctorMainSpecialty} onChange={(e) => setDoctorMainSpecialty(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Doctor Age</Label>
-                                <Input type="text" value={doctorAge} onChange={(e) => setDoctorAge(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Patient Age</Label>
-                                <Input type="text" value={patientAge} onChange={(e) => setPatientAge(e.target.value)} />
-                            </div>
-                            <div className="flex flex-col w-1/2">
-                                <Label>Patient Gender</Label>
-                                <Select>
-                                    <SelectTrigger>
-                                        <SelectValue>{patientGender}</SelectValue>
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem onClick={() => setPatientGender('Male')}>Male</SelectItem>
-                                        <SelectItem onClick={() => setPatientGender('Female')}>Female</SelectItem>
-                                        <SelectItem onClick={() => setPatientGender('Other')}>Other</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
-                        </div>
+						<div className="flex w-full flex-row gap-12 px-10">
+							<div className="flex flex-col w-1/2">
+								<Label>Appointment Status</Label>
+								<Select
+									required
+									onValueChange={setAppointmentStatus}
+									defaultValue={appointmentStatus}
+								>
+									<SelectTrigger>
+										<SelectValue placeholder="Select Status">
+											{appointmentStatus}
+										</SelectValue>
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem value="Complete">Complete</SelectItem>
+										<SelectItem value="Queued">Queued</SelectItem>
+										<SelectItem value="Serving">Serving</SelectItem>
+										<SelectItem value="Cancel">Cancel</SelectItem>
+										<SelectItem value="NoShow">No Show</SelectItem>
+										<SelectItem value="Skip">Skip</SelectItem>
+									</SelectContent>
+								</Select>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Appointment Type</Label>
+								<Select>
+									<SelectTrigger>
+										<SelectValue>{appointmentType}</SelectValue>
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem
+											onClick={() => setAppointmentType('Consultation')}
+										>
+											Consultation
+										</SelectItem>
+										<SelectItem onClick={() => setAppointmentType('Inpatient')}>
+											Inpatient
+										</SelectItem>
+									</SelectContent>
+								</Select>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Time Queued</Label>
+								<DatePicker
+									mode="single"
+									selected={timeQueued}
+									onSelect={setTimeQueued}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Queue Date</Label>
+								<Input
+									type="text"
+									value={queueDate}
+									onChange={(e) => setQueueDate(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Start Time</Label>
+								<Input
+									type="text"
+									value={startTime}
+									onChange={(e) => setStartTime(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>End Time</Label>
+								<Input
+									type="text"
+									value={endTime}
+									onChange={(e) => setEndTime(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Virtual</Label>
+								<Select>
+									<SelectTrigger>
+										<SelectValue>{virtual}</SelectValue>
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem onClick={() => setVirtual('True')}>
+											True
+										</SelectItem>
+										<SelectItem onClick={() => setVirtual('False')}>
+											False
+										</SelectItem>
+									</SelectContent>
+								</Select>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Hospital Name</Label>
+								<Input
+									type="text"
+									value={hospitalName}
+									onChange={(e) => setHospitalName(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>City</Label>
+								<Input
+									type="text"
+									value={city}
+									onChange={(e) => setCity(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Province</Label>
+								<Input
+									type="text"
+									value={province}
+									onChange={(e) => setProvince(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Region</Label>
+								<Input
+									type="text"
+									value={region}
+									onChange={(e) => setRegion(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Doctor Main Specialty</Label>
+								<Input
+									type="text"
+									value={doctorMainSpecialty}
+									onChange={(e) => setDoctorMainSpecialty(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Doctor Age</Label>
+								<Input
+									type="text"
+									value={doctorAge}
+									onChange={(e) => setDoctorAge(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Patient Age</Label>
+								<Input
+									type="text"
+									value={patientAge}
+									onChange={(e) => setPatientAge(e.target.value)}
+								/>
+							</div>
+							<div className="flex flex-col w-1/2">
+								<Label>Patient Gender</Label>
+								<Select>
+									<SelectTrigger>
+										<SelectValue>{patientGender}</SelectValue>
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem onClick={() => setPatientGender('Male')}>
+											Male
+										</SelectItem>
+										<SelectItem onClick={() => setPatientGender('Female')}>
+											Female
+										</SelectItem>
+										<SelectItem onClick={() => setPatientGender('Other')}>
+											Other
+										</SelectItem>
+									</SelectContent>
+								</Select>
+							</div>
+						</div>
 					</div>
 					<DialogFooter>
 						{loading ? (
-							<Button disabled>
-								Please wait
-							</Button>
+							<Button disabled>Please wait</Button>
 						) : (
 							<Button type="submit" className="mt-6">
 								Create Post
